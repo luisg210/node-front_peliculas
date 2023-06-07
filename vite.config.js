d8072c3722cd6f95b2e6173a2abf4bc7469a7706
luisg210 <gerardo.henriquez21@itca.edu.sv>
2023-06-07 13:50:06 -0600

    Agregando script en package.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://luisg210.github.io/node-front/",
+  base: "https://github.com/luisg210/node-front/",
   plugins: [react()],
 })
